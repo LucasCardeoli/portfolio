d248173e4854e693f852035366a8280b0ce3abdc
Zuurike <lucascarvalhodeoliveira37@gmail.com>
2025-05-20 14:38:03 -0300

    Project created

---
+++ src/main/java/com/lucascardeoli/artlist/ArtlistApplication.java
@@ -0,0 +1,13 @@
+package com.lucascardeoli.artlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtlistApplication.class, args);
+	}
+
+}
